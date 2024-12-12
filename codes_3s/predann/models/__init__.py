55fd5865c2be37390efdd07109ffbe3c0c3a5e89
JURIUENO11 <158554713+JURIUENO11@users.noreply.github.com>
2024-12-12 11:30:45 +0900

    Delete codes_3s directory

--- codes_3s/predann/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model import Model
-from .sample_cnn2d_eeg import SampleCNN2DEEG
--- codes_3s/predann/modules/__init__.py
+++
@@ -1,2 +0,0 @@
-from .clip_loss import CLIP_Loss, CLIP_Loss_New
-from .contrastive_learning_3s import EEGContrastiveLearning
